2c910c4562c1698021aeec6a3f51dc8b34f4af01
scutLaoYi <scutlaoyi@gmail.com>
2014-03-30 21:49:11 +0800

    Update: page sentence
        New sentence added.

--- add_ons/pagesentence/sentence.js
+++ add_ons/pagesentence/sentence.js
@@ -40,7 +40,8 @@ var sentences = new Array(
         "逝川与流光，飘忽不相待。",
         "只有两种事物是无穷无尽的--宇宙和人类的愚蠢。但对于前者，我不敢确定。--爱因斯坦",
         "And a person's character, isn't determined by how he or she enjoys vitory, but rather how he or she endures defeat.Nothing can help us endure dark times better than our faith.",
-        "只有自己足够好，才不会惧怕任何模样的未来。"
+        "只有自己足够好，才不会惧怕任何模样的未来。",
+        "即使岩石再坚硬，也是死的，鸡蛋再脆弱，也是活着的生命。岩石最终会碎成细沙， 而鸡蛋中就孵化越过岩石。"
 
         );
 function generateSentence()
